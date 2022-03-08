bdf7fbbce867050629a20bff80e1215c8ac60df5
Alkan <91989029+alkan-iztech@users.noreply.github.com>
2022-03-09 00:01:56 +0300

    simple registration

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from recom import process_csv, get_cos_sim, recommend
 import os
+from validators import validate_password, validate_user_data
 
 app = Flask(__name__)
 app.config['SESSION_PERMANENT'] = False
@@ -72,6 +73,41 @@ print(df.iloc[indices])
 @app.route('/')
 def home():
     return make_response('HOME', 200)
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    try:
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        password_confirm = request.form['password-confirm']
+        validation_result = validate_user_data(db, User, username, email, password, password_confirm)
+        if validation_result == '':
+            data = User(username, email, password)
+            db.session.add(data)
+            db.session.commit()
+            return custom_message(data.id, 200)
+        return custom_message(validation_result, 404)
+    except Exception as e:
+        return custom_message(e, 404)
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            return custom_message(user.id, 200)
+        else:
+            return custom_message('Incorrect email or password', 404)
+
+    except Exception as e:
+        return custom_message(e, 404)
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    return custom_message('Logged out', 200)
 
 def custom_message(message, status_code): 
     return make_response(jsonify(message), status_code)
