c8891f4c1a4a50a6da36879139157b0a6afa0efb
Alkan <91989029+alkan-iztech@users.noreply.github.com>
2022-03-08 23:47:41 +0300

    define classes

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import pandas as pd
-import scipy.sparse as sp
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, make_response, jsonify
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from recom import process_csv, get_cos_sim, recommend
+import os
 
 app = Flask(__name__)
 app.config['SESSION_PERMANENT'] = False
@@ -11,67 +10,71 @@ app.config['SESSION_TYPE'] = "filesystem"
 app.debug = True
 Session(app)
 
+BOOK_RECOM_ENV = os.environ.get("THREE_ONE_EIGHT_ENV")
+if BOOK_RECOM_ENV == 'prod':
+    app.debug = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("JAWSDB_URL")
+else:
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/three_one_eight'
+
+db = SQLAlchemy(app)
+
+@app.before_first_request
+def setup():
+    db.create_all()
+class User(db.model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+class App(db.model):
+    __tablename__ = 'apps'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    first_item_id = db.Column(db.Integer)
+
+    def __init__(self, name, user_id, first_item_id):
+        self.name = name
+        self.user_id = user_id
+        self.first_item_id = first_item_id
+        
+class AppItem(db.model):
+    __tablename__ = 'app_items'
+    id = db.Column(db.Integer, primary_key=True)
+    concat_data = db.Column(db.String(300), nullable=False)
+    app_id = db.Column(db.Integer, db.ForeignKey('apps.id'))
+    # cos_sim_row = db.Column(db.LargeBinary)
+    
+    def __init__(self, concat_data, app_id) -> None:
+        self.concat_data = concat_data
+        self.app_id = app_id
+
 df = None
 cos_sim = None
-
-def process_csv(csv, seperator=','):
-    print('Loading data...')
-    df = pd.read_csv(csv, sep=seperator, on_bad_lines='skip')
-    df = df.head(20000)
-
-    df = df.dropna(how='any')
-    df = df.drop_duplicates(subset='title', keep='first')
-    # df.head(5)
-    return df
-
-def get_cos_sim(df):
-    combined = df[df.columns[2:-1]].apply(
-        lambda x: ','.join(x.dropna().astype(str)),
-        axis=1
-    ).str.lower().replace({"[^A-Za-z0-9 ]+": ""}, regex=True)
-    combined = combined.reset_index(drop=True)
-    # combined.head()
-
-    #################################
-    #################################
-
-    count = CountVectorizer(stop_words='english')
-    count_matrix = count.fit_transform(combined)
-
-    #     tfidf = TfidfVectorizer(stop_words='english')
-    #     tfidf_matrix = tfidf.fit_transform(data_plot['subtopics'])
-
-    #     combine_sparse = sp.hstack([count_matrix, tfidf_matrix], format='csr')
-
-    count2 = CountVectorizer(stop_words='english')
-    count_matrix2 = count2.fit_transform(df[df.columns[-1]].apply(lambda x: x[1:-1].replace(',', ' ')))
-    combine_sparse = sp.hstack([count_matrix, count_matrix2], format='csr')
-
-    cos_sim = cosine_similarity(combine_sparse, combine_sparse)
-
-    print('Loaded data.')
-    return cos_sim
-
-def recommend(title, df, cos_sim):
-    indices = pd.Series(df.index, index = df['title'])
-    index = indices[title]
-    print(index)
-
-    sim_scores = list(enumerate(cos_sim[index]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:10]
-
-    indices = [i[0] for i in sim_scores]
-    return indices
 
 df = process_csv('./items20000.csv')
 cos_sim = get_cos_sim(df)
 indices = recommend('Red Queen 1', df, cos_sim)
 print(df.iloc[indices])
 
+###########################
+########### API ###########
+###########################
 @app.route('/')
 def home():
-    return "HOME"
+    return make_response('HOME', 200)
+
+def custom_message(message, status_code): 
+    return make_response(jsonify(message), status_code)
 
 if __name__ == '__main__':
     app.run()
